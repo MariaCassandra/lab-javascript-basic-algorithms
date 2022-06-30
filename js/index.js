1fd7b924c538bac190db3d81cd801f96de6edec7
Maria Cassandra <maria.cassandra@gmail.com>
2022-06-30 21:44:33 +0200

    Iteration 1 finished

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "Code Master";
+console.log("The driver's name is " + hacker1);
+
+let hacker2 = "Extreme navigator";
+console.log("The navigator's name is " + hacker2);
+
+
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
+
+
+// console.log("I'm ready!");
