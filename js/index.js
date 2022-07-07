2def7362ac875e10d2e8840c001ad6960e17a7e1
Maria Cassandra <73235842+MariaCassandra@users.noreply.github.com>
2022-07-07 19:07:53 +0200

    Bonus 1 done

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ if(orderName === -1) {
   console.log("What?! You both have the same name?")
 }
 
-/* Bonus 1:
+// Bonus 1:
 
 let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum posuere pulvinar. In hac habitasse platea dictumst. Curabitur commodo, risus placerat bibendum hendrerit, sapien orci eleifend magna, sit amet porta odio purus ut metus. Proin commodo arcu enim, eu volutpat lectus bibendum et. Maecenas elementum ultricies neque, et venenatis elit vulputate et. In a tincidunt nunc. Aenean lacinia at ipsum vel varius. Curabitur euismod orci ac rutrum maximus. Phasellus ac viverra orci. Etiam luctus nulla ut quam feugiat, vel auctor eros congue. Ut rutrum commodo elit, et faucibus velit feugiat nec. Sed eget erat non lorem interdum auctor. Donec libero justo, lacinia ac convallis ut, feugiat vitae est.
 
@@ -48,8 +48,10 @@ Mauris erat diam, semper vel sapien ac, luctus vulputate ligula. Praesent cursus
 
 Nunc molestie consectetur tempor. In in ipsum purus. Fusce ac laoreet elit, in porttitor tortor. Quisque pulvinar nunc vitae augue tristique blandit. Fusce aliquet quam sit amet semper suscipit. Integer quis lectus mollis, commodo sem eu, bibendum elit. Donec ac sapien dignissim, egestas sem sit amet, convallis dui. Quisque at laoreet tortor. In hac habitasse platea dictumst. Donec tellus justo, lacinia a suscipit venenatis, euismod at tellus. Duis dignissim velit lacinia neque sodales pretium. Nunc convallis urna quis sapien cursus, at pellentesque tellus hendrerit. Nulla malesuada ipsum nisl, volutpat sodales arcu efficitur ac."
 
-function countOfWords() {
-
+function countOfWords(lorem) {
+  	let numberOfWords = lorem.split(" ").length;
+	  return numberOfWords;
 }
+console.log(countOfWords(lorem));
 
-// Bonus 2: */
+// Bonus 2: 
